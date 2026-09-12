917746be39bb79c8fffc45d5c449dbcd8c0dd960
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-12 20:01:08 +0530

    Implement user management functionality in script.js with form submission handling

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let form = document.querySelector("form");
-let userName = document.querySelector("#name");
+let username = document.querySelector("#name");
 let role = document.querySelector("#role");
 let bio = document.querySelector("#bio");
 let photo = document.querySelector("#photo");
 
+let userManager = {
+    user: [],
+
+    init: function(){
+        form.addEventListener('submit', this.submitForm.bind(this));
+    },
+
+    submitForm: function(e){
+        e.preventDefault();
+        this.addUser();
+    }, 
+
+    addUser: function(){
+        this.user.push({
+            username: username.value,
+            role: role.value,
+            bio: bio.value,
+            photo: photo.value,
+        });
+
+        form.reset();
+    }
+
+}
