cf75327748e11a3787359e4e5042ad87bf129bae
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-12 20:40:51 +0530

    Refactor user management functions in script.js for improved readability and functionality

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ let photo = document.querySelector("#photo");
 let userManager = {
     user: [],
 
-    init: function(){
+    init: function () {
         form.addEventListener('submit', this.submitForm.bind(this));
     },
 
-    submitForm: function(e){
+    submitForm: function (e) {
         e.preventDefault();
         this.addUser();
-    }, 
+    },
 
-    addUser: function(){
+    addUser: function () {
         this.user.push({
             username: username.value,
             role: role.value,
@@ -25,6 +25,48 @@ let userManager = {
         });
 
         form.reset();
-    }
+        this.renderUI();
+    },
+
+    renderUI: function () {
+        document.querySelector(".users").innnerHTML = "";
+        this.user.forEach(function (user) {
+            let card = document.createElement("div");
+            card.className = "bg-white/90 backdrop-blur rounded-2xl shadow-xl p-8 flex flex-col items-center border border-blue-100 hover:scale-105 transition";
+
+            const img = document.createElement("img");
+            img.className =
+                "w-28 h-28 rounded-full object-cover mb-5 border-4 border-blue-200 shadow";
+            img.src = user.photo;
+            img.alt = "User Photo";
+            card.appendChild(img);
+
+            // Name
+            const name = document.createElement("h2");
+            name.className = "text-2xl font-bold mb-1 text-blue-700";
+            name.textContent = user.username;
+            card.appendChild(name);
+
+            // Role
+            const role = document.createElement("p");
+            role.className = "text-purple-500 mb-2 font-medium";
+            role.textContent = user.role;
+            card.appendChild(role);
+
+            // Description
+            const desc = document.createElement("p");
+            desc.className = "text-gray-700 text-center";
+            desc.textContent = user.bio;
+            card.appendChild(desc);
+
+            document.querySelector(".users").appendChild(card);
+        });
+    },
+
+    removeUser: function(){
+
+    },
 
 }
+
+userManager.init();
