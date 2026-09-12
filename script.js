7b5622e554aa77ca49ee75e838b1347dc4866905
mohammadjawed01 <mohammadjaved1736@gmail.com>
2026-09-12 19:52:03 +0530

    Add form element selectors in script.js for user input fields

---
+++ script.js
@@ -0,0 +1,6 @@
+let form = document.querySelector("form");
+let userName = document.querySelector("#name");
+let role = document.querySelector("#role");
+let bio = document.querySelector("#bio");
+let photo = document.querySelector("#photo");
+
